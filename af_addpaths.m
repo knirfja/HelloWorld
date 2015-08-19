9a80e64bd91ad1c67b320a73840ad61a3ae8a91c
Andrew <knirfja@yahoo.com>
2015-08-19 15:17:48 -0500

    Matlabfile

---
+++ af_addpaths.m
@@ -0,0 +1,43 @@
+% af_AddpathScript
+%
+%
+%% Golding Lab Comp
+% Add generic scripts
+addpath('C:\Users\frink\OneDrive\OneNoteMatlab')
+
+% Add latest Schnitzcell and subfolders
+addpath(genpath('C:\Users\frink\OneDrive\OneNoteMatlab\Golding\SchnitzCell\Schnitzcells20150306'))
+
+% Add latest Spatzcells
+addpath(genpath('C:\Users\frink\OneDrive\OneNoteMatlab\Golding\SpatzCell'))
+
+% Add Lambda scripts
+% addpath('C:\Users\frink\OneDrive\OneNoteMatlab\Golding\Lambda')
+addpath('C:\Users\frink\OneDrive\OneNoteMatlab\Golding\Lambda')
+
+% Add 20150216_parsinf
+% addpath(genpath('C:\Users\frink\OneDrive\OneNoteMatlab\Golding\Lambda\20150216_parsinf'))
+% addpath(genpath('C:\Users\frink\OneDrive\OneNoteMatlab\Golding\Lambda\20150311mgparsab'))
+
+% Add 20150311_mgparsab
+addpath(genpath('C:\Users\frink\Documents\MATLAB\Lambda\20150311_mgparsab'))
+
+%% Home Comp
+% Add generic scripts
+addpath('F:\OneDrive\OneNoteMatlab')
+
+% Add latest Schnitzcell and subfolders
+addpath(genpath('F:\OneDrive\OneNoteMatlab\Golding\SchnitzCell\Schnitzcells20150306'))
+
+% Add latest Spatzcells
+% addpath(genpath('F:\OneDrive\OneNoteMatlab\Golding\SpatzCell'))
+
+% Add Lambda scripts
+% addpath('F:\OneDrive\OneNoteMatlab\Golding\Lambda')
+
+% Add 20150216_parsinf
+addpath(genpath('F:\OneDrive\OneNoteMatlab\Golding\Lambda\20150216_parsinf'))
+
+% Add 20150311mgparsab
+addpath(genpath('F:\OneDrive\OneNoteMatlab\Golding\Lambda\20150311mgparsab'))
+
